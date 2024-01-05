09102a1f02bc134523c7cf36421cd7dc15f3ab29
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-01-05 23:07:38 +0100

    --wip-- [skip ci]

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { VStack } from "./components/ui/stack";
+import { Center, VStack } from "./components/ui/stack";
 import { Buttons } from "./demo/button.stories";
 import { Comboboxes } from "./demo/combobox.stories";
 import { Inputs } from "./demo/input.stories";
 
 function App() {
   return (
-    <VStack>
-      <Buttons />
-      <Inputs />
-      <Comboboxes />
-    </VStack>
+    <Center className="w-screen flex-1">
+      <p class="text-2xl">Arek UI - Solid</p>
+      <VStack
+        spacing={5}
+        className="md:flex-row flex-wrap pb-20 justify-center pt-5 px-5"
+      >
+        <Buttons />
+        <Inputs />
+        <Comboboxes />
+      </VStack>
+    </Center>
   );
 }
 
